b39bc6700525cdf43b21f0b9e98d6d36435c401c
Poker44 <devnull@poker44.local>
2026-03-09 23:24:25 +0100

    fix

--- poker44/base/validator.py
+++ poker44/base/validator.py
@@ -1,6 +1,5 @@
 # The MIT License (MIT)
 # Copyright © 2023 Yuma Rao
-# (developer): Miguelik
 # Copyright © 2025 Poker44 Subnet
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
